1a56e03c3034423d2634105e0535da36314100bb
U-HOME\Kyle <coda2@illinois.edu>
2015-12-15 23:42:16 -0600

    updated

--- LeagueAPI.js
+++ LeagueAPI.js
@@ -1,4 +1,6 @@
 var apikey = "fa1cdffc-714b-4d5e-9dbf-a53c8df2db66";
+var sList = new Array();
+var mList = new Array();
 
 function startup(){
     //IDfromSummonerName("Lil Guy");
@@ -21,12 +23,8 @@ function startup(){
         var IDlist = [];
         var namelist = [];
         GetListOfChallengers(IDlist, namelist);
-        GetMasteries(champID, namelist, IDlist);
-        //var sumname = NameFromID("138243");
-        //console.log(sumname);
-        //console.log(IDlist);
-        //console.log(namelist);
-     
+        GetListOfGames(champID, namelist, IDlist);
+        createSelect();
     }
 
 }
@@ -92,17 +90,19 @@ function GetChampID(champname){
     
 }
 
-function GetMasteries(champID, nameList, idlist){
-    var sList = [];
-    var mList = [];
-    for(var i=0; i < idlist.length; i++){
+function GetListOfGames(champID, nameList, idlist){
+    for(var i=0; i < idlist.length && i < 20; i++){
         var xhr = new XMLHttpRequest(); 
-        xhr.open("GET", "https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/"+ idlist[i] + "?championIds="+ champID + "&rankedQueues=RANKED_SOLO_5x5&seasons=PRESEASON2016&api_key" + apikey, false);
+        xhr.open("GET", "https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/"+idlist[i]+"?championIds="+champID+"&rankedQueues=RANKED_SOLO_5x5&seasons=PRESEASON2016&api_key=" + apikey, false);
         xhr.send();
         
+        
+        sleep(1000);
+        console.log("current summoner: " + nameList[i]);
         var response = xhr.responseText;
         var matchIDs = GetMatchIDs(response);
         if(matchIDs.length >0){
+            console.log("found games");
             sList.push(nameList[i]);
             mList.push(matchIDs);
         }
@@ -116,8 +116,9 @@ function GetMasteries(champID, nameList, idlist){
 
 
 function GetMatchIDs(response){
-   
-    var key = "matchID";
+    
+    //console.log(response);
+    var key = "matchId";
     var list = [];
     var currmatchidx = 0;
     var currcommaidx = 0;
@@ -131,15 +132,39 @@ function GetMatchIDs(response){
     return list;
 }
     
-function NameFromID(ID){
-    var xhr = new XMLHttpRequest(); 
-    xhr.open("GET", "https://na.api.pvp.net/api/lol/na/v1.4/summoner/" + ID + "api_key" + apikey, false);
-    xhr.send();
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+function createSelect(){
     
-    var response = xhr.responseText;
-    var nameidx = response.indexOf("name");
-    var theid = response.substring(nameidx+2, response.indexOf(",", nameidx)-1);
-    return theid;
+    var select = document.createElement("SELECT");
+    select.id = "summoner";
+    document.body.appendChild(select);
+    for(var i=0; i < sList.length; i++){
+        var option = document.createElement("option");
+        option.text = sList[i];
+        option.value = sList[i];
+        select.add(option);
+    }
+    var button = document.createElement("BUTTON");
+    button.textContent = "Get Masteries";
+    button.onclick = function() { 
+        var summ = document.getElementById("summoner").value;
+        GetMasteries(summ);
+    }
+    document.body.appendChild(button);
+}
+    
+    
+function GetMasteries(summ){
+    
+    console.log(summ);
 }
     
     
